4d9458aa1b43b569d611fe13b2106ab8249ca8b0
益昇 石 <ndmc402010104@gmail.com>
2026-06-22 15:51:12 +0800

    06221551

--- android/app/src/main/java/com/jonaminz/skhps/MainActivity.java
+++ android/app/src/main/java/com/jonaminz/skhps/MainActivity.java
@@ -1,5 +1,34 @@
 package com.jonaminz.skhps;
 
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+
+    if (this.bridge != null && this.bridge.getWebView() != null) {
+      WebView webView = this.bridge.getWebView();
+      WebSettings settings = webView.getSettings();
+
+      settings.setTextZoom(100);
+      settings.setUseWideViewPort(true);
+      settings.setLoadWithOverviewMode(false);
+      settings.setBuiltInZoomControls(false);
+      settings.setDisplayZoomControls(false);
+      settings.setSupportZoom(false);
+    }
+  }
+
+  @Override
+  public void onBackPressed() {
+    if (this.bridge != null && this.bridge.getWebView() != null && this.bridge.getWebView().canGoBack()) {
+      this.bridge.getWebView().goBack();
+    } else {
+      super.onBackPressed();
+    }
+  }
+}
